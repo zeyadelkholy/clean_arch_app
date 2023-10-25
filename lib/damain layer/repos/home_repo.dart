84aa6f7271871893765edb3412c5ec21eb976d5e
Zeyad <zeyadelkholy>
2023-10-26 01:48:46 +0200

    add Either class

--- lib/damain layer/repos/home_repo.dart
+++ lib/damain layer/repos/home_repo.dart
@@ -1,9 +1,10 @@
+import 'package:clean_arch_booklyapp/core/errors/app_failures.dart';
 import 'package:clean_arch_booklyapp/damain%20layer/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo{
 
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
-
+  Future<Either< Failure , List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either< Failure , List<BookEntity>>> fetchNewestBooks();
 
 }
