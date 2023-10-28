adcd488c792853a3b95261951c6150b2a859337a
Zeyad <zeyadelkholy>
2023-10-28 23:22:44 +0200

    implement remote data in fetch featured books

--- lib/data layer/data_sources/home_remote_data_souce.dart
+++ lib/data layer/data_sources/home_remote_data_souce.dart
@@ -1,3 +1,6 @@
+import 'package:clean_arch_booklyapp/core/api%20service/api_service.dart';
+import 'package:clean_arch_booklyapp/data%20layer/models/book_model/book_model.dart';
+
 import '../../damain layer/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -7,10 +10,18 @@ abstract class HomeRemoteDataSource {
 }
 
 class HomeRemoteDataSourceImplementation extends HomeRemoteDataSource{
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImplementation(this.apiService);
+
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+  var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&q=programming');
+  List<BookEntity> books =[];
+  for( var bookMap in data['item']){
+        books.add( BookModel.fromJson(bookMap));
+  }
+    return books;
   }
 
   @override
