8213ae6920059f3ce1c89b6f5a29b7ea3f535634
Zeyad <zeyadelkholy>
2023-10-29 11:49:15 +0200

    register adapter and open featured box

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
 import 'Core/utils/service_locator.dart';
 import 'core/utils/App_router.dart';
+import 'damain layer/entities/book_entity.dart';
 
-void main() {
+void main() async {
 
   // to call the register Singleton from service locator
   setupServiceLocator();
   runApp(const BooklyApp());
+  Hive.registerAdapter(BookEntityAdapter());
+  await  Hive.openBox('featured_box');
 }
 
 class BooklyApp extends StatelessWidget {
