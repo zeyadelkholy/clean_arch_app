fd19580cd3860d04eb223d6a5275a3fd835e817c
Zeyad <zeyadelkholy>
2023-11-01 10:29:43 +0200

    create server failure from dio error

--- lib/core/errors/app_failures.dart
+++ lib/core/errors/app_failures.dart
@@ -11,6 +11,7 @@ class ServerFailure extends Failure {
 
   factory ServerFailure.fromDioError(DioException dioError) {
     switch (dioError.type) {
+
       case DioExceptionType.connectionTimeout:
         return ServerFailure('Connection timeout with ApiServer');
 
@@ -20,11 +21,17 @@ class ServerFailure extends Failure {
       case DioExceptionType.receiveTimeout:
         return ServerFailure('Receive timeout with ApiServer');
 
+
+
+
       case DioExceptionType.badResponse:
         return ServerFailure.fromResponse(
             dioError.response!.statusCode, dioError.response!.data);
       case DioExceptionType.cancel:
         return ServerFailure('Request to ApiServer was canceld');
+
+
+
 
       case DioExceptionType.unknown:
         if (dioError.message!.contains('SocketException')) {
@@ -36,15 +43,26 @@ class ServerFailure extends Failure {
     }
   }
 
+
+
   factory ServerFailure.fromResponse(int? statusCode, dynamic response) {
+
     if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+
       return ServerFailure(response['error']['message']);
+
     } else if (statusCode == 404) {
       return ServerFailure('Your request not found, Please try later!');
+
     } else if (statusCode == 500) {
       return ServerFailure('Internal Server error, Please try later');
+
     } else {
       return ServerFailure('Opps There was an Error, Please try again');
     }
   }
+}
+
+class CacheFailure extends Failure{
+  CacheFailure(super.errorMessage);
 }
--- lib/data layer/repositories/home_screen_repo_implementation.dart
+++ lib/data layer/repositories/home_screen_repo_implementation.dart
@@ -1,105 +1,62 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-import '../../Core/api service/api_service.dart';
 import '../../Core/errors/app_failures.dart';
-import '../models/book_model/book_model.dart';
-import 'home_screen_repo.dart';
+import '../../damain layer/entities/book_entity.dart';
+import '../../damain layer/repos/home_repo.dart';
+import '../data_sources/home_local_data_source.dart';
+import '../data_sources/home_remote_data_souce.dart';
 
-class HomeRepoImpl implements HomeRepo {
-  final ApiService apiService;
 
-  HomeRepoImpl(this.apiService);
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImpl(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBooks() async {
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks(
+      {int pageNumber = 0}) async {
+    List<BookEntity> booksList;
+
     try {
-      var data = await apiService.get(
-          endPoint:
-          'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
+      booksList = homeLocalDataSource.fetchFeaturedBooks();
 
-      // for loop for every item in data
-      List<BookModel> books = [];
-      for (var item in data['items']) {
-        try {
-          // هعمل check وارجع اكمل تاني
-          books.add(BookModel.fromJson(item));
-        } catch (e) {
-          books.add(BookModel.fromJson(item));
-        }
+      if (booksList.isNotEmpty) {
+        return right(booksList);
       }
+      booksList =  await homeRemoteDataSource.fetchFeaturedBooks();
 
-      return right(books);
+      return right(booksList);
     } catch (e) {
+
+
       if (e is DioException) {
-        return left(
-          ServerFailure.fromDioError(e),
-        );
+        return left(ServerFailure.fromDioError(e));
       }
-      return left(
-        ServerFailure(
-          e.toString(),
-        ),
-      );
+      return left(ServerFailure(e.toString()));
     }
   }
 
-  @override
-  Future<Either<Failure, List<BookModel>>> fetchNewestBooks()  async {
-    try {
-      var data = await apiService.get(
-          endPoint:
-          'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
-      List<BookModel> books = [];
-      for (var item in data['items']) {
-        try {
-          books.add(BookModel.fromJson(item));
-        } catch (e) {
-          books.add(BookModel.fromJson(item));
-        }
-      }
 
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return right(books);
+      }
+      books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } catch (e) {
       if (e is DioException) {
-        return left(
-          ServerFailure.fromDioError(e),
-        );
+        return left(ServerFailure.fromDioError(e));
       }
-      return left(
-        ServerFailure(
-          e.toString(),
-        ),
-      );
-    }
-  }
-
-  @override
-  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async {
-    try {
-      var data = await apiService.get(
-          endPoint:
-          'volumes?Filtering=free-ebooks&Sorting=relevance &q=subject:Programming');
-      List<BookModel> books = [];
-      for (var item in data['items']) {
-        try {
-          books.add(BookModel.fromJson(item));
-        } catch (e) {
-          books.add(BookModel.fromJson(item));
-        }
-      }
-
-      return right(books);
-    } catch (e) {
-      if (e is DioException) {
-        return left(
-          ServerFailure.fromDioError(e),
-        );
-      }
-      return left(
-        ServerFailure(
-          e.toString(),
-        ),
-      );
+      return left(ServerFailure(e.toString()));
     }
   }
 }
